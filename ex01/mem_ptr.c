2efddd8694de171c8c4c3e53dc3a40a827ba02ec
Julien Ferrier <jferrier@localhost.localdomain>
2019-01-04 15:57:32 +0100

    [modify] the exercice 2 is now correct

--- ex01/mem_ptr.c
+++ ex01/mem_ptr.c
@@ -14,12 +14,12 @@ void add_str(const char *str1, const char *str2, char **res)
 {
     *res = malloc(my_strlen(str1) + (my_strlen(str2)) + 1);
     my_strcpy(*res, str1);
-    my_strcpy(*res, str2);
+    strcat(*res, str2);
 }
 
 void add_str_struct(str_op_t *str_op)
 {
-    add_str(str_op->str1, str_op->str2, str_op->&res);
+    add_str(str_op->str1, str_op->str2, &str_op->res);
 }
 
 int my_strlen(char const *str)
