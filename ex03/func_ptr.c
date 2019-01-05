427b5f2bd8d8feaf7a694b5ed8dd3e2e40ba7450
Julien Ferrier <jferrier@localhost.localdomain>
2019-01-05 11:09:48 +0100

    [modify] delete the main of the ex3

--- ex03/func_ptr.c
+++ ex03/func_ptr.c
@@ -5,8 +5,7 @@
 ** funct_ptr.c
 */
 
-//#include "func_ptr_enum.h"
-#include "funct_ptr.h"
+#include "func_ptr.h"
 #include <string.h>
 
 
@@ -50,24 +49,6 @@ void print_42(const char *str)
     printf("42\n");
 }
 
-/*void do_action(action_t action, const char *str)
-{
-    int i = 0;
-    t_action_func list[] = {
-        {PRINT_NORMAL, &print_normal},
-        {PRINT_REVERSE, &print_reverse},
-        {PRINT_UPPER, &print_upper},
-        {PRINT_42, &print_42},
-    };
-    while (i < 5) {
-        if (action == list[i].action) {
-            (*list[i].func)(str);
-            return;
-        }
-        i++;
-    }
-    }*/
-
 void do_action(action_t action, const char *str)
 {
     void (*f[])(const char *) = {
@@ -77,15 +58,4 @@ void do_action(action_t action, const char *str)
         print_42
     };
     f[action](str);
-}
-
-int main(void)
-{
-    char *str = "hello je suis en tek2d";
-
-    do_action(PRINT_NORMAL, str);
-    do_action(PRINT_REVERSE, str);
-    do_action(PRINT_UPPER, str);
-    do_action(PRINT_42, str);
-    return (0);
 }
